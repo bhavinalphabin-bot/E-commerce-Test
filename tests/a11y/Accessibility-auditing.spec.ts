310a13876b8401d63ee1a3aa55d26335315628cc
bhavy207AB <bhavy.alphabin@gmail.com>
2026-06-11 11:48:23 +0530

    Login email field is keyboard accessible

---
+++ tests/a11y/Accessibility-auditing.spec.ts
@@ -0,0 +1,14 @@
+import test, { expect } from "@playwright/test";
+
+
+test('Login email field is keyboard accessible', async ({ page }) => {
+  await page.goto('https://automationexercise.com/login');
+
+  const loginEmail = page.locator(
+    'input[data-qa="login-email"]'
+  );
+
+  await loginEmail.focus();
+
+  await expect(loginEmail).toBeFocused();
+});
